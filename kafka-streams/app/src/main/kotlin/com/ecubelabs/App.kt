131c344466efaed9e87c767260f92b7a049fe4f8
titaniper <flutter.kr@gmail.com>
2024-06-19 14:44:30 +0900

    feat: Kotlin + Gradle 기반으로 프로젝트 세팅

---
+++ kafka-streams/app/src/main/kotlin/com/ecubelabs/App.kt
@@ -0,0 +1,68 @@
+package com.ecubelabs
+
+import com.ecubelabs.config.KafkaStreamConfig
+import com.ecubelabs.partitioner.MessagePartitioner
+import com.ecubelabs.transformer.MessageBroker
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.processor.ProcessorSupplier
+import java.util.*
+import java.util.regex.Pattern
+
+class App {
+    val greeting: String
+        get() {
+            return "Hello World!"
+        }
+}
+
+fun initConfig(): KafkaStreamConfig {
+    return KafkaStreamConfig(
+            arrayOf<String>(System.getenv("KAFKA_BROKER")),
+            System.getenv("KAFKA_SOURCE_TOPICS").split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray(),
+            System.getenv("KAFKA_SINK_TOPICS").split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    )
+}
+
+fun initProperties(config: KafkaStreamConfig): Properties {
+    val props = Properties()
+    props[StreamsConfig.APPLICATION_ID_CONFIG] = "message-broker"
+    props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = config.getBrokers().joinToString(",")
+    props[StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG] = Serdes.String().javaClass.name
+    props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = Serdes.String().javaClass.name
+    // TODO: 이것도 동적으로 설정할 수 있지 않을까?
+    props[StreamsConfig.producerPrefix(ProducerConfig.PARTITIONER_CLASS_CONFIG)] = MessagePartitioner::class.java.getName()
+    return props;
+}
+
+fun initTopology(config: KafkaStreamConfig): Topology {
+    val builder = StreamsBuilder()
+    val topology = builder.build()
+    // NOTE: Source 설정
+    for (sourceTopic in config.getSourceTopics()) {
+        topology.addSource("Source", Pattern.compile(sourceTopic))
+    }
+
+    // TODO: 이것도 동적으로 설정할 수 있지 않을까?
+    topology.addProcessor("Process", ProcessorSupplier { MessageBroker() }, "Source")
+
+    // NOTE: Sink 설정
+    for (sinkTopic in config.getSinkTopics()) {
+        topology.addSink<String, String>(sinkTopic, sinkTopic, Serdes.String().serializer(), Serdes.String().serializer(), "Process")
+    }
+
+    return topology;
+}
+
+fun main() {
+    val config = initConfig();
+    val streams = KafkaStreams(initTopology(config), initProperties(config))
+
+    streams.start()
+
+    Runtime.getRuntime().addShutdownHook(Thread { streams.close() })
+}
